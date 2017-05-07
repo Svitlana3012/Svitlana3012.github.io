173f84638fa94ddd5d21cf7dab1e910cdf8f8d47
Svitlana3012 <drobashko@yahoo.com>
2017-05-07 20:16:28 +0300

    first task

---
+++ js-hw-1/script.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+  function calculate(a, b) {
+    var result = a;
+
+    if (b >= 0) {
+    for (var i = 1; i < b; i++) {
+      result *= a;
+    }
+  return (result);
+
+  } else {
+    for (var i = 1; i < b; i++) {
+      result = 1 / result * a;
+    }
+  return (result);
+}
+  }
+
+  var num = prompt(`Введите число для просчета`);
+  var pow = prompt(`Введите значение степени для просчета`);
+  var result = calculate(num, pow);
+  console.log(`result is: `, result);
+
+})();
