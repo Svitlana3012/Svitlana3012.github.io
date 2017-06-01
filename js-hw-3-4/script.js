0f7c642920fcba098372eb0d228bd2bc4a8eda74
Svitlana3012 <drobashko@yahoo.com>
2017-06-01 13:13:07 +0300

    without before

--- js-hw-3-4/script.js
+++ js-hw-3-4/script.js
@@ -1,53 +1,60 @@
-(function () {
+(function() {
   'use strict';
 
-  var div = document.createElement('div');
-  div.classList.add('root');
-  var test = document.createElement('test');
-  test.appendChild(document.createTextNode('Тест по программированию'));
-  div.appendChild(test);
+  var root = document.createElement('div');
+  root.classList.add('root');
 
-  var data = ['...'];
-  var obj = {
+  var body = document.querySelector('body');
+  body.appendChild(root);
 
+  var test = document.createElement('div');
+  test.classList.add('test');
+  root.appendChild(test);
+
+  var title = document.createElement('h3');
+  title.classList.add('title');
+  title.appendChild(document.createTextNode('Тест по программированию'));
+  test.appendChild(title);
+
+  var wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+  test.appendChild(wrapper);
+
+  var data = [{
+      questionNumber: '1. Вопрос #1',
+      answerVariant: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3']
+    },
+    {
+      questionNumber: '2. Вопрос #2',
+      answerVariant: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3', 'Вариант овтета 4']
+    },
+    {
+      questionNumber: '3. Вопрос #3',
+      answerVariant: ['Вариант овтета 1', 'Вариант овтета 2']
+    }
+  ];
+
+  var questions = document.createDocumentFragment();
+
+
+  for (var i = 0, max = data.length; i < max; i++) {
+    var question = document.createElement('ul');
+    question.appendChild(document.createTextNode(data[i].questionNumber));
+    questions.appendChild(question);
+
+    for (var a = 0, length = data[i].answerVariant.length; a < length; a++) {
+      var answer = document.createElement('li');
+      answer.appendChild(document.createTextNode(data[i].answerVariant[a]));
+      question.appendChild(answer);
+    }
   }
 
-  var questionNumber = ['Вопрос №1', 'Вопрс №2', 'Вопрос №3'];
-  var questions = document.createDocumentFragment();
-  for (var i=0, max=questionNumber.length; i<max; i++) {
-   var question = document.createElement('ul');
-   question.appendChild(document.createTextNode(questionNumber[i]));
-   questions.appendChild(question);
-}
-  test.appendChild(questions);
+  wrapper.appendChild(questions);
 
-  var answerVariant = ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3', 'Вариант овтета 4'];
-  var answers = document.createDocumentFragment();
-  for (var a=0, length=answerVariant.length; a<length; a++) {
-   var answer = document.createElement('li');
-   answer.appendChild(document.createTextNode(answerVariant[a]));
-   answers.appendChild(answer);
-}
-  questions.appendChild(answers);
+  var button = document.createElement('button');
+  button.classList.add('check');
+  button.appendChild(document.createTextNode('Проверить мои результаты'));
 
-  // var test = {
-  //   data: {
-  //     title: 'Тест по какой-то теме',
-  //     questions: [
-  //       {
-  //         title: 'Вопрос #1',
-  //         answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3']
-  //       },
-  //       {
-  //         title: 'Вопрос #2',
-  //         answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3', 'Вариант овтета 4']
-  //       },
-  //       {
-  //         title: 'Вопрос #3',
-  //         answers: ['Вариант овтета 1', 'Вариант овтета 2']
-  //       }
-  //     ]
-  //   }
-  // };
+  test.appendChild(button);
 
 })();
