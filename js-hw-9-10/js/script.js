0c37b68298503e6e023a0408b034c8f6a28b4a60
Svitlana3012 <drobashko@yahoo.com>
2017-07-11 16:38:00 +0300

    carousel

---
+++ js-hw-9-10/js/script.js
@@ -0,0 +1,47 @@
+//Carousel plugin
+$(function() {
+  'use strict';
+
+    $('.jcarousel').jcarousel({
+        animation: 'slow'
+    })
+    .jcarouselAutoscroll({
+    interval: 3000,
+    target: '+=1',
+    autostart: true
+});
+
+    $('.jcarousel').jcarousel({
+    animation: {
+        duration: 800,
+        easing:   'linear',
+        complete: function() {
+        }
+    }
+  });
+
+  $('.jcarousel').jcarouselAutoscroll({
+    target: '+=3'
+});
+
+  $('.jcarousel').jcarousel('scroll', target);
+
+  $('.jcarousel').jcarousel('scroll', '+=2');
+
+  $('.jcarousel-prev').jcarouselControl({
+    target: '-=1',
+    carousel: carousel
+});
+
+$('.jcarousel-next').jcarouselControl({
+    target: '+=1',
+    carousel: carousel
+});
+
+$('.jcarousel-pagination').jcarouselPagination({
+    item: function(page) {
+        return '<a href="#' + page + '">' + page + '</a>';
+    }
+});
+
+});
